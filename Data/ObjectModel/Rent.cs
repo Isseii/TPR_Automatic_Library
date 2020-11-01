001920ae6bf9f0874e88380a51fde661d2d19159
Isseii <sebastian.kujawski.98@poczta.fm>
2020-11-01 22:57:27 +0100

    Part of Logic Layer functionalities

--- Data/ObjectModel/Rent.cs
+++ Data/ObjectModel/Rent.cs
@@ -8,8 +8,8 @@ namespace Automatic_Library
         public DateTime RentalDate { get; set; }
         public Reader Reader { get; set; }
         public Guid Id { get; set; }
-
         public BookCopy Book { get; set; }
+
         public Rent(BookCopy book, Reader reader, DateTime rentalDate)
         {
             this.Book = book;
---
+++ Logic/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Automatic_Library.Logic
+{
+    class DataService
+    {
+        private DataRepository _dataRepository;
+        
+        IEnumerable<BookDescription> AllBookDescriptions()
+        {
+            return _dataRepository.GetAllBookDescriptions();
+        }
+
+        IEnumerable<Rent> ReaderRents(Reader reader)
+        {
+            return _dataRepository.GetAllRents().Where(x => x.Reader == reader);
+        }
+
+        IEnumerable<Rent> RentsBetweenDates(DateTime start, DateTime end)
+        {
+            return _dataRepository.GetAllRents().Where( x => x.RentalDate > start && x.RentalDate < end);
+        }
+
+        Rent AddRent(BookCopy bookCopy, Reader reader)
+        {
+            if(_dataRepository.GetReader(reader.Id) == null || _dataRepository.GetBookCopy(bookCopy.Id) == null)
+            {
+                return null;
+            }
+            Rent rent = new Rent(bookCopy, reader, DateTime.Now);
+            _dataRepository.AddRent(rent);
+            return rent;
+        }
+        
+
+    }
+}
