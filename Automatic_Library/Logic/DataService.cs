92df06887af66c80d3e5f7c29c4ef0aac5a1b6a2
Dominik Karski <dominik-karski@wp.pl>
2020-11-08 13:16:48 +0100

    DataService functionalities added. IDataService interface added.

--- Automatic_Library/Logic/DataService.cs
+++ Automatic_Library/Logic/DataService.cs
@@ -7,30 +7,118 @@ using Automatic_Library.Data.ObjectModel.BookEvent;
 
 namespace Automatic_Library.Logic
 {
-    class DataService
+    public class DataService : IDataService
     {
         private AbstractDataRepository _dataRepository;
-        
-        IEnumerable<BookDescription> AllBookDescriptions()
+
+        public IEnumerable<KeyValuePair<string, BookDescription>> GetAllBookDescriptions()
         {
-            throw new NotImplementedException();
+            return _dataRepository.GetAllBookDescriptions();
         }
 
-        IEnumerable<BookEvent> ReaderBookEvents(Reader reader)
+        public IEnumerable<BookEvent> GetAllBookEvents()
         {
+            return _dataRepository.GetAllBookEvents();
+        }
+
+        public IEnumerable<BookCopy> GetAllBookCopies()
+        {
+            return _dataRepository.GetAllBookCopies();
+        }
+
+        public IEnumerable<Reader> GetAllReaders()
+        {
+            return _dataRepository.GetAllReaders();
+        }
+
+        public bool isBookDescriptionInDataRepository(BookDescription bookDescription)
+        {
+            return GetAllBookDescriptions().Where(x => x.Equals(bookDescription)).Count() != 0;
+        }
+
+        public bool isBookEventInDataRepository(BookEvent bookEvent)
+        {
+            return GetAllBookEvents().Where(x => x.Equals(bookEvent)).Count() != 0;
+        }
+        public bool isBookCopyInDataRepository(BookCopy bookCopy)
+        {
+            return GetAllBookCopies().Where(x => x.Equals(bookCopy)).Count() != 0;
+        }
+        public bool isReaderInDataRepository(string name, string lastName)
+        {
+            var reader = new Reader(name, lastName);
+            return GetAllReaders().Where(x => x.Equals(reader)).Count() != 0;
+        }
+
+        public IEnumerable<BookEvent> ReaderBookEvents(string name, string lastName)
+        {
+            var reader = new Reader(name, lastName);
             return _dataRepository.GetAllBookEvents().Where(x => x.Reader == reader);
         }
 
-        IEnumerable<BookEvent> BookEventsBetweenDates(DateTime start, DateTime end)
+        public IEnumerable<BookEvent> BookEventsBetweenDates(DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            return GetAllBookEvents().Where(x => x.EventTime >= start && x.EventTime <= end);
         }
 
-        BookEvent AddBookEvent(BookCopy bookCopy, Reader reader)
+        public void AddBookCopy(string title, string author, string publisher, string isbn)
         {
-            throw new NotImplementedException();
+            var bookDescription = new BookDescription(title, author, publisher);
+            var bookCopy = new BookCopy(bookDescription, DateTime.Now, isbn);
+            _dataRepository.AddBookCopy(bookCopy);
         }
-        
+
+        public void AddBookDescription(string title, string author, string publisher)
+        {
+            var bookDescription = new BookDescription(title, author, publisher);
+            _dataRepository.AddBookDescription(bookDescription);
+        }
+
+        public void RentBook(Reader reader, BookCopy bookCopy)
+        {
+            var rent = new Rent(bookCopy, reader, DateTime.Now);
+            _dataRepository.AddBookEvent(rent);
+        }
+
+        public void ReturnBook(Reader reader, BookCopy bookCopy)
+        {
+            var returnBook = new Return(bookCopy, reader, DateTime.Now);
+            _dataRepository.AddBookEvent(returnBook);
+        }
+
+        public void AddReader(string name, string lastName)
+        {
+            var reader = new Reader(name, lastName);
+            _dataRepository.AddReader(reader);
+        }
+
+        public void DeleteBookCopy(string isbn)
+        {
+            foreach (var bookCopy in _dataRepository.GetAllBookCopies())
+            {
+                if (bookCopy.Isbn.Equals(isbn))
+                {
+                    _dataRepository.DeleteBookCopy(bookCopy);
+                }
+            }
+        }
+
+        public void DeleteBookDescription(string title)
+        {
+            var bookDescription = _dataRepository.GetBookDescription(title);
+            _dataRepository.DeleteBookDescription(bookDescription);
+        }
+
+        public void DeleteReader(string name, string lastName)
+        {
+            var reader = new Reader(name, lastName);
+            _dataRepository.DeleteReader(reader);
+        }
+
+        public void DeleteBookEvent(BookEvent bookEvent)
+        {
+            _dataRepository.DeleteBookEvent(bookEvent);
+        }
 
     }
 }
---
+++ Automatic_Library/Logic/IDataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Automatic_Library.Data.ObjectModel;
+using Automatic_Library.Data.ObjectModel.BookEvent;
+
+namespace Automatic_Library.Logic
+{
+    public interface IDataService
+    {
+        void AddBookCopy(string title, string author, string publisher, string isbn);
+        void AddBookDescription(string title, string author, string publisher);
+        void AddReader(string name, string lastName);
+        IEnumerable<BookEvent> BookEventsBetweenDates(DateTime start, DateTime end);
+        void DeleteBookCopy(string isbn);
+        void DeleteBookDescription(string title);
+        void DeleteBookEvent(BookEvent bookEvent);
+        void DeleteReader(string name, string lastName);
+        IEnumerable<BookCopy> GetAllBookCopies();
+        IEnumerable<KeyValuePair<string, BookDescription>> GetAllBookDescriptions();
+        IEnumerable<BookEvent> GetAllBookEvents();
+        IEnumerable<Reader> GetAllReaders();
+        bool isBookCopyInDataRepository(BookCopy bookCopy);
+        bool isBookDescriptionInDataRepository(BookDescription bookDescription);
+        bool isBookEventInDataRepository(BookEvent bookEvent);
+        bool isReaderInDataRepository(string name, string lastName);
+        IEnumerable<BookEvent> ReaderBookEvents(string name, string lastName);
+        void RentBook(Reader reader, BookCopy bookCopy);
+        void ReturnBook(Reader reader, BookCopy bookCopy);
+    }
+}
