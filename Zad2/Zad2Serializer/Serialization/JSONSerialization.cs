265b9c1596e0ad6beea74f47df1336fd46e6ed5f
Dominik Karski <dominik-karski@wp.pl>
2020-11-26 20:45:15 +0100

    JSONSerialization class and partial test class implementation.

---
+++ Zad2/Zad2Serializer/Serialization/JSONSerialization.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Zad2Serializer.Serialization
+{
+    public class JSONSerialization<T>
+    {
+        private string _fileName;
+        private T _obj;
+        private JsonSerializerSettings _jsonSettings;
+
+        public JSONSerialization(string fileName, T obj)
+        {
+            _fileName = fileName;
+            _obj = obj;
+            _jsonSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All,
+                PreserveReferencesHandling = PreserveReferencesHandling.All
+            };
+        }
+
+        public void serialize()
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+
+            string json = JsonConvert.SerializeObject(_obj, Formatting.Indented, _jsonSettings);
+            File.WriteAllText(_fileName, json);
+        }
+
+        public T deserialize()
+        {
+            if (!File.Exists(_fileName))
+            {
+                throw new FileNotFoundException();
+            }
+
+            string json = File.ReadAllText(_fileName);
+            T deserializedObject = JsonConvert.DeserializeObject<T>(json, _jsonSettings);
+            return deserializedObject;
+        }
+    }
+}
---
+++ Zad2/Zad2SerializerTest/JSONSerializerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zad2Serializer.ObjectModel;
+using Zad2Serializer.Serialization;
+using System.IO;
+
+namespace Zad2SerializerTest
+{
+    [TestClass]
+    public class JSONSerializerTest
+    {
+        private A a;
+        private B b;
+        private C c;
+
+        [TestInitialize()]
+        public void SetUp()
+        {
+            A a = new A("Dominick", "Carski", 2137, new DateTime(2019, 12, 1), null);
+            B b = new B("Sebaschan", "Kijawiński", 9669, new DateTime(2019, 10, 1), null);
+            C c = new C("Michał", "Karbowany", 5321, new DateTime(2020, 1, 2), null);
+
+            a.ObjB = b;
+            b.ObjC = c;
+            c.ObjA = a;
+        }
+
+        [TestMethod]
+        public void AObjSerializationTest()
+        {
+            string fileName = "ASerializationTest.json";
+            JSONSerialization<A> serializer = new JSONSerialization<A>(fileName, a);
+            serializer.serialize();
+
+            string serializedContent = File.ReadAllText(fileName);
+            //Assert.AreEqual(serializedContent, @".");
+        }
+    }
+}
