6733d47fa067b7e2a3b7710903bc22ac686f6d2b
Isseii <heliosxd12345@gmail.com>
2020-12-29 18:40:58 +0100

    Addition of few QueryTools tests

--- Zad3DatabaseLINQTest/QueryToolsTests.cs
+++ Zad3DatabaseLINQTest/QueryToolsTests.cs
@@ -24,29 +24,41 @@ namespace Zad3DatabaseLINQ.Tests
         [TestMethod()]
         public void GetProductsByVendorNameTest()
         {
-            string name = "Australia Bike Retailer";
-            var tmp = QueryTools.GetProductNamesByVendorName(name);
+            string name = "Victory Bikes";
+            var tmp = QueryTools.GetProductsByVendorName(name);
 
-            Assert.AreEqual(tmp.Count ,16);
-            Assert.AreEqual(tmp[0], "Thin-Jam Lock Nut 9");
+            Assert.AreEqual(tmp.Count, 4);
+            Assert.AreEqual(tmp[0].Name, "Road Tire Tube");
+
         }
 
         [TestMethod()]
         public void GetProductNamesByVendorNameTest()
         {
+            string name = "Australia Bike Retailer";
+            var tmp = QueryTools.GetProductNamesByVendorName(name);
 
+            Assert.AreEqual(tmp.Count, 16);
+            Assert.AreEqual(tmp[0], "Thin-Jam Lock Nut 9");
         }
 
         [TestMethod()]
         public void GetProductVendorByProductNameTest()
         {
-    
+            string name = "LL Shell";
+            var tmp = QueryTools.GetProductVendorByProductName(name);
+
+            Assert.AreEqual(tmp, "Federal Sport");
+
+
         }
 
         [TestMethod()]
         public void GetProductsWithNRecentReviewsTest()
         {
-     
+            var tmp = QueryTools.GetProductsWithNRecentReviews(2);
+
+            Assert.AreEqual(tmp.Count, 1);
         }
 
         [TestMethod()]
